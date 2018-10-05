52eef79e6eff660e70beca4a8ff0a1e4bdb6022f
tamil <tamilselvan.sk@gmail.com>
2018-10-05 15:14:39 +0530

    Initial Commit

---
+++ gitdemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gitdemo
+//
+//  Created by tamilselvan on 05/10/18.
+//  Copyright © 2018 tamilselvan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
